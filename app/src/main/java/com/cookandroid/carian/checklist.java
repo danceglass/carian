08a5ea1c70494cfebb5c799a7b937d1d693ff466
문다형 <danceglass@gmail.com>
2020-12-06 12:52:45 +0900

    - 체크리스트 기능 일부 구현

--- app/src/main/java/com/cookandroid/carian/checklist.java
+++ app/src/main/java/com/cookandroid/carian/checklist.java
@@ -3,6 +3,8 @@ package com.cookandroid.carian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class checklist extends AppCompatActivity {
 
@@ -10,5 +12,51 @@ public class checklist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checklist);
+
+        final ToggleButton tb3 =
+                (ToggleButton) this.findViewById(R.id.toggleButton2);
+
+        tb3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                if(tb3.isChecked()){
+                    tb3.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.bgon)
+                    );
+                }else{
+                    tb3.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.bgoff)
+                    );
+                } // end if
+            } // end onClick()
+        });
+
+
+        final ToggleButton tb2 =
+                (ToggleButton) this.findViewById(R.id.toggleButton2);
+
+        tb2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                if(tb2.isChecked()){
+                    tb2.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.bton)
+                    );
+                }else{
+                    tb2.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.btoff)
+                    );
+                } // end if
+            } // end onClick()
+        });
+
     }
 }
